e7e3ab042a6d4465dc2c440821fb9532ce02f4c7
anjalisi <muskan.anjali.singh@gmail.com>
2020-09-22 22:26:06 +0530

    Changing Registration Layout and linking login and signup

--- app/src/main/java/com/example/attendancetracker/Login.java
+++ app/src/main/java/com/example/attendancetracker/Login.java
@@ -2,13 +2,29 @@ package com.example.attendancetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    Button goToSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Redirect to signup
+        goToSignUp=(Button)findViewById(R.id.signupButton);
+        goToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUp();
+            }
+        });
+    }
+    public void openSignUp(){
+        Intent signupp= new Intent(this, Signup.class);
+        startActivity(signupp);
+        
     }
 }
--- app/src/main/java/com/example/attendancetracker/Signup.java
+++ app/src/main/java/com/example/attendancetracker/Signup.java
@@ -3,12 +3,20 @@ package com.example.attendancetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Signup extends AppCompatActivity {
-
+    Spinner dept;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        //Adding drop down
+        dept= (Spinner)findViewById(R.id.department);
+        ArrayAdapter<String> deptList= new ArrayAdapter<String>(Signup.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.departments));
+        deptList.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dept.setAdapter(deptList);
     }
 }
