30d15af5db8ac1b7be2a45794608c5d2fdef30bb
anjalisi <muskan.anjali.singh@gmail.com>
2020-09-22 21:39:52 +0530

    Changing login layout

--- app/src/main/java/com/example/attendancetracker/MainActivity.java
+++ app/src/main/java/com/example/attendancetracker/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.attendancetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Animation logoAnim;
+    ImageView logo;
+    //adding a new screen after splash
+    private static int SPLASH_SCREEN=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Making the splash appear in full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        logo=(ImageView)findViewById(R.id.logoImg);
+        //Adding splash fade in animation
+        logoAnim= AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.logo_animation);
+        logo.setAnimation(logoAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent login = new Intent(MainActivity.this, Login.class);
+                startActivity(login);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
